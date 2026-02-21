f7363506cab515eb0d2f9054867194a8312bf5dd
Tara Pande <tarande57@gmail.com>
2026-02-21 15:09:14 -0800

    finished hw7

--- regression/logreg.py
+++ regression/logreg.py
@@ -119,7 +119,7 @@ class LogisticRegressor(BaseRegressor):
     
     def make_prediction(self, X) -> np.array:
         """
-        TODO: Implement logistic function to get estimates (y_pred) for input X values. The logistic
+        Implement logistic function to get estimates (y_pred) for input X values. The logistic
         function is a transformation of the linear model into an "S-shaped" curve that can be used
         for binary classification.
 
@@ -129,11 +129,13 @@ class LogisticRegressor(BaseRegressor):
         Returns: 
             The predicted labels (y_pred) for given X.
         """
-        pass
+        z = X @ self.W                  # Linear model
+        y_pred = 1 / (1 + np.exp(-z))   # Sigmoid transformation
+        return y_pred
     
     def loss_function(self, y_true, y_pred) -> float:
         """
-        TODO: Implement binary cross entropy loss, which assumes that the true labels are either
+        Implement binary cross entropy loss, which assumes that the true labels are either
         0 or 1. (This can be extended to more than two classes, but here we have just two.)
 
         Arguments:
@@ -143,11 +145,15 @@ class LogisticRegressor(BaseRegressor):
         Returns: 
             The mean loss (a single number).
         """
-        pass
+        eps = 1e-9                                  #avoid log(0) -> undefined
+        y_pred = np.clip(y_pred, eps, 1 - eps)      #bound the min/max range of y_pred 
+
+        loss = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))    #binary cross entropy loss
+        return loss
         
     def calculate_gradient(self, y_true, X) -> np.ndarray:
         """
-        TODO: Calculate the gradient of the loss function with respect to the given data. This
+        Calculate the gradient of the loss function with respect to the given data. This
         will be used to update the weights during training.
 
         Arguments:
@@ -157,4 +163,8 @@ class LogisticRegressor(BaseRegressor):
         Returns: 
             Vector of gradients.
         """
-        pass
+        y_pred = self.make_prediction(X)            #make prediction given feature matrix X
+        n = X.shape[0]                              #number of samples
+
+        grad = (X.T @ (y_pred - y_true)) / n        #gradient equation
+        return grad
--- test/test_logreg.py
+++ test/test_logreg.py
@@ -10,16 +10,59 @@ This is not an exhaustive list.
 
 # Imports
 import pytest
+from regression.utils import loadDataset
+from regression.logreg import LogisticRegressor
+import numpy as np
 # (you will probably need to import more things here)
 
 def test_prediction():
-	pass
+	X, y = loadDataset()								#load dataset
+	model = LogisticRegressor(num_feats=X.shape[1])		#load model with same feature # as in data
+
+	X_pad = np.hstack([X, np.ones((X.shape[0], 1))])	#pad data with ones (bias term)
+
+	z = X_pad @ model.W									#linear model (Y = WX)
+	expected = 1 / (1 + np.exp(-z))						#sigmoid function (manual = expected)
+
+	y_pred = model.make_prediction(X_pad)				#my code's prediction
+
+	assert np.allclose(y_pred, expected)				#assert prediction is close to expected
 
 def test_loss_function():
-	pass
+	X, y = loadDataset()								#load dataset
+	model = LogisticRegressor(num_feats=X.shape[1])		#load model with same feature # as in data
+
+	X_pad = np.hstack([X, np.ones((X.shape[0], 1))])	#pad data with ones (bias term)
+	y_pred = model.make_prediction(X_pad)				#make prediction
+
+	eps = 1e-9											#avoid log(0) -> undefined
+	y_pred_clip = np.clip(y_pred, eps, 1 - eps)			#bound the min/max range of y_pred 
+
+	expected_loss = -np.mean(y * np.log(y_pred_clip) + (1 - y) * np.log(1 - y_pred_clip))	#manual loss function = expected
+	loss = model.loss_function(y, y_pred)				#my code's binary cross entropy loss
+
+	assert np.isclose(loss, expected_loss)				#assert loss is close to expected
 
 def test_gradient():
-	pass
+	X, y = loadDataset()								#load dataset
+	model = LogisticRegressor(num_feats=X.shape[1])		#load model with same feature # as in data
+
+	X_pad = np.hstack([X, np.ones((X.shape[0], 1))])	#pad data with ones (bias term)
+	y_pred = model.make_prediction(X_pad)				#make prediction
+
+	expected_grad = (X_pad.T @ (y_pred - y)) / X_pad.shape[0]	#manual gradient equation = expected
+	grad = model.calculate_gradient(y, X_pad)			#my code's gradient function
+
+	assert np.allclose(grad, expected_grad)				#assert gradient is close to expected
 
 def test_training():
-	pass
+	X_train, X_test, y_train, y_test = loadDataset(split_percent=0.8)	#load dataset (split 80% train and 20% test)
+	model = LogisticRegressor(num_feats=X_train.shape[1], max_iter=5)	#load model with same feature # as in data
+
+	initial_weights = model.W.copy()						#save my initial model weights
+	model.train_model(X_train, y_train, X_test, y_test)		#train the model
+
+	assert not np.allclose(initial_weights, model.W)		#when model trains, the weights should change significantly!
+
+	assert len(model.loss_hist_train) > 0				#the loss history should not be empty
+	assert len(model.loss_hist_val) > 0
